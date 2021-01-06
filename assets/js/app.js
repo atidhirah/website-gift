0adf9a9c7174fc96bd6fca16ecaecbb3caf9969b
banyuwangi1998 <atidhirabanyuwangi@gmail.com>
2021-01-06 09:43:36 +0700

    Make navigation bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  setupNavigation();
+});
+
+const setupNavigation = () => {
+  const navMenu = document.getElementById("nav-menu");
+  const navToggle = document.getElementById("nav-toggle");
+  const navLinks = document.querySelectorAll(".nav__link");
+
+  // Open and hide menu via nav toggle
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("show-menu");
+  });
+
+  // Hide menu after clicking link
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      navMenu.classList.remove("show-menu");
+    });
+  });
+};
