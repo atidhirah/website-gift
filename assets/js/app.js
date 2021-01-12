3b9881ee85347233598eec0efd9ddfae90d9b597
banyuwangi1998 <atidhirabanyuwangi@gmail.com>
2021-01-12 10:45:32 +0700

    set scroll eventlistener on navigation bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,7 +19,48 @@ const setupNavigationBar = () => {
     });
   });
 
+  // Change active link on scrolling
+  document.addEventListener("scroll", () => scrollActive());
+
+  // Change header bg when scrolling
+  document.addEventListener("scroll", () => headerActive());
+
   // Setup the dark mode button
+  setupDarkModeButton();
+};
+
+const scrollActive = () => {
+  const sections = document.querySelectorAll("section[id]");
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((section) => {
+    const sectionHeight = section.offsetHeight;
+    const sectionTop = section.offsetTop - 50;
+    const sectionId = section.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+};
+
+const headerActive = () => {
+  const header = document.querySelector("header");
+
+  if (this.scrollY >= 200) {
+    header.classList.add("active-header");
+  } else {
+    header.classList.remove("active-header");
+  }
+};
+
+const setupDarkModeButton = () => {
   const navModeButton = document.getElementById("nav-darkmode");
   const darkTheme = "dark-theme";
   const iconTheme = "bxs-sun";
